b782ab163091e038823dbbbf041aa42770a8f48c
chenwenhua <wenhua.chen@versa-ai.com>
2018-07-22 10:43:41 +0800

    贴纸组件

---
+++ src/components/ImageSticker.vue
@@ -0,0 +1,73 @@
+<style scoped>
+.image-sticker {
+  position: absolute;
+  box-sizing: border-box;
+  border: 1px solid #333
+}
+</style>
+
+<template>
+  <div class="image-sticker" :style="style">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    config: {
+      type: Object,
+      default: () => ({
+        draggable: true, // 是否可拖拽
+        deleteable: true, // 是否可以删除
+      }) 
+    },
+    state: {
+      type: Object,
+      default: () => ({
+        width: 0,
+        height: 0,
+        left: 0,
+        top: 0,
+        rotate: 0,
+        zIndex: 0,
+        isActive: false
+      })
+    }
+  },
+  data: function () {
+    return {
+      name: 'imageSticker',      
+      width: 100,
+      height: 100,
+      left: 10,
+      top: 10,
+      rotate: 10,
+      zIndex: 0,
+      isActive: false      
+    }
+  },
+  computed: {
+    style () {
+      return {
+        top: this.top + 'px',
+        left: this.left + 'px',
+        width: this.width + 'px',
+        height: this.height + 'px',
+        zIndex: this.zIndex,
+        transform: `rotate(${this.rotate}deg)`
+      }
+    }
+  },
+  methods: {
+    
+  },
+  created: function () {
+    console.log('created', this)
+  },
+  mounted: function () {
+    console.log('mounted', this)
+  },
+  updated: function () {
+    console.log('updated', this)
+  }
+}
+</script>
---
+++ src/views/Demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h1>This is an demo page</h1>
+    <div class="demo-main">
+      <image-sticker></image-sticker> 
+    </div>     
+  </div>
+</template>
+<script>
+import ImageSticker from '@/components/ImageSticker.vue'
+export default {
+  components: {
+    ImageSticker
+  },
+  data: function () {
+    return {      
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+.demo-main {
+  position: relative;
+  width: 600px;
+  height: 400px;
+  border: 1px solid red;
+}
+</style>
